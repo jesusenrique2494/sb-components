111a2a8d8124ee99849f59512b5fc18971086f38
Jesus Enrique <jesusenrique2494@gmail.com>
2021-11-29 19:22:37 -0300

    primer commit de storybook

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,39 @@
+import './mylabel.css';
+
+export interface MyLabelProps{
+    /**
+     * El nombre de label que vas a usar
+    */
+    label:string;
+    /**
+     * El tamaño que vas a usar en el label
+    */
+    size:'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * El color de la letra
+    */
+    fontColor?:string;
+    /**
+     * La capitalizacion del label
+    */
+    allCaps?:boolean;
+    /**
+     * El color del label
+    */
+    color?:'primary' | 'secondary' | 'tertiary'
+
+}
+
+export const MyLabel = ({
+    label='no label',
+    size='normal',
+    allCaps=false,
+    color='primary',
+    fontColor
+    }:MyLabelProps) => {
+    return (
+        <span className={`label ${size} text-${color} ${allCaps && 'capitalize' }`} style={{color:fontColor}} >
+            {allCaps? label.toLocaleUpperCase(): label}
+        </span>
+    )
+}
