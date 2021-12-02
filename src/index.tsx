f9a6f9899e914174a3b12b08685da5a03f331924
Jesus Enrique <jesusenrique2494@gmail.com>
2021-12-01 22:27:08 -0300

    feat: elimine error en index

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,3 @@ export { Button } from './stories/Button';
 export { Header } from './stories/Header';
 export { MyLabel } from './components/MyLabel';
 export { Page } from './stories/Page';
-
-GH_TOKEN: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
-
-NPM_TOKEN: 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
